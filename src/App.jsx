2c0b5efdb8b9eaa9aa838d4182564ad53eaaa6cc
Bryanwangzb <bryanwzb@163.com>
2021-11-05 18:24:07 +0900

    build GraphQL database schma

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { AmplifySignOut,withAuthenticator } from '@aws-amplify/ui-react';
-import Amplify from 'aws-amplify'
+import Amplify,{API} from 'aws-amplify'
 import awsConfig from './aws-exports'
 
 Amplify.configure(awsConfig)
 
 function App() {
+
+  const fetchSongs = async () => {
+    try{
+      const songData = await API.graphql(graphqlOperation(listSongs))
+
+    }catch(error){
+
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <AmplifySignOut />
-        <h2>My App Content</h2>
+        <h2>Kozipro App</h2>
       </header>
     </div>
   );
