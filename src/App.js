d9ca0568ebbdc5f4b22eee064988b5ee193d6aad
Bryanwangzb <bryanwzb@163.com>
2021-11-05 17:33:30 +0900

    update app.js for testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { AmplifySignOut,withAuthenticator } from '@aws-amplify/ui-react';
-import Amplify from '@aws-amplify';
+import Amplify from 'aws-amplify'
 import awsConfig from './aws-exports'
 
 Amplify.configure(awsConfig)
