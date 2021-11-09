025587640ac8cd8d0739fc1fc178de49670938d5
Bryanwangzb <bryanwzb@163.com>
2021-11-09 09:15:45 +0900

    add upload mp3 file function.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import Amplify, { API, Storage } from 'aws-amplify'
 import awsConfig from './aws-exports'
 import { listSongs } from './graphql/queries'
-import { updateSong, updateSongs } from './graphql/mutations'
+import { updateSong, createSong} from './graphql/mutations'
 import { useState } from 'react'
 import { useEffect } from 'react'
 
 import ReactPlayer from 'react-player'
+import {v4 as uuid } from 'uuid'
 import { graphqlOperation } from '@aws-amplify/api'
 import { Paper, IconButton, TextField } from '@material-ui/core'
 import PlayArrowIcon from '@material-ui/icons/PlayArrow'
@@ -148,42 +149,49 @@ export default withAuthenticator(App);
 
 
 const AddSong = ({ onUpload }) => {
-
   const [songData, setSongData] = useState({});
+  const [mp3Data, setMp3Data] = useState();
 
+  const uploadSong = async () => {
+      //Upload the song
+      console.log('songData', songData);
+      const { title, description, owner } = songData;
 
+      const { key } = await Storage.put(`${uuid()}.mp3`, mp3Data, { contentType: 'audio/mp3' });
 
-  const uploadSong = () => {
-
-    // Upload the song
-    console.log('songData',songData);
-    onUpload();
-  }
-
+      const createSongInput = {
+          id: uuid(),
+          title,
+          description,
+          owner,
+          filePath: key,
+          like: 0,
+      };
+      await API.graphql(graphqlOperation(createSong, { input: createSongInput }));
+      onUpload();
+  };
 
   return (
-    <>
       <div className="newSong">
-        <TextField
-          label="Title" 
-          value={songData.title} 
-          onChange={e => setSongData({ ...songData, tilte: e.target.value })}
+          <TextField
+              label="Title"
+              value={songData.title}
+              onChange={e => setSongData({ ...songData, title: e.target.value })}
           />
-        <TextField
-          label="Artist"
-          value={songData.owner} 
-          onChange={e => setSongData({ ...songData, owner: e.target.value })}
-        />
-        <TextField
-          label="Description"
-          value={songData.description}
-          onChange={e => setSongData({...songData,description:e.target.value})}
-        />
-        <input type="file" accept="audio/mp3" onChange={e => setMp3Data(e.target.files[0])} />
-        <IconButton onClick={uploadSong}>
-          <PublishIcon />
-        </IconButton>
+          <TextField
+              label="Artist"
+              value={songData.owner}
+              onChange={e => setSongData({ ...songData, owner: e.target.value })}
+          />
+          <TextField
+              label="Description"
+              value={songData.description}
+              onChange={e => setSongData({ ...songData, description: e.target.value })}
+          />
+          <input type="file" accept="audio/mp3" onChange={e => setMp3Data(e.target.files[0])} />
+          <IconButton onClick={uploadSong}>
+              <PublishIcon />
+          </IconButton>
       </div>
-    </>
-  )
-}
+  );
+};
