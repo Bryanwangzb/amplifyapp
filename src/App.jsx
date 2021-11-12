95646eac1579d8bc2bf33e96b65ce91a36fd53bf
Bryanwangzb <bryanwzb@163.com>
2021-11-13 00:04:44 +0900

    Add Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-//@ts-check
 
 import logo from './logo.svg';
 import './App.css';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
-import Amplify, { API, Storage,Auth } from 'aws-amplify'
+import Amplify, { API, Storage, Auth } from 'aws-amplify'
 import awsConfig from './aws-exports'
 import SongList from './components/SongList'
-import {Button} from '@material-ui/core'
+import { Button } from '@material-ui/core'
+// import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import {Switch,Route,BrowserRouter as Router} from 'react-router-dom'
+
 
 Amplify.configure(awsConfig)
 
 function App() {
 
-  const signOut = async() =>{
-    try{
+  const signOut = async () => {
+    try {
       await Auth.signOut()
-    }catch(error){
-      console.log('error signing out',error)
+    } catch (error) {
+      console.log('error signing out', error)
     }
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button variant="contained" color="primary" onClick={signOut}>
-          Sign Out
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Button variant="contained" color="primary" onClick={signOut}>
+            Sign Out
           </Button>
-        <h2>Kozipro App</h2>
-      </header>
-        <SongList />
-    </div>
+          <h2>Kozipro App</h2>
+        </header>
+      <Switch>
+        <Route exact path="/">
+          <SongList />
+        </Route>
+      </Switch>
+       
+        {/* <Switch>
+          <Route exact path='/'>
+          <SongList />
+        </Switch> */}
+
+      </div>
+    </Router>
   );
 }
 
