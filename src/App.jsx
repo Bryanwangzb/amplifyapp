1194be98fba1f51d61264ee8f78d4c15b08eae15
Bryanwangzb <bryanwzb@163.com>
2021-11-12 23:42:27 +0900

    Change sign out button style.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
+//@ts-check
+
 import logo from './logo.svg';
 import './App.css';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
-import Amplify, { API, Storage } from 'aws-amplify'
+import Amplify, { API, Storage,Auth } from 'aws-amplify'
 import awsConfig from './aws-exports'
 import SongList from './components/SongList'
+import {Button} from '@material-ui/core'
 
 Amplify.configure(awsConfig)
 
 function App() {
 
-  
+  const signOut = async() =>{
+    try{
+      await Auth.signOut()
+    }catch(error){
+      console.log('error signing out',error)
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <AmplifySignOut />
+        <Button variant="contained" color="primary" onClick={signOut}>
+          Sign Out
+          </Button>
         <h2>Kozipro App</h2>
       </header>
         <SongList />
